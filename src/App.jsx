971adb68b5a297ec81b7734b11beb575ac4ca4cd
Syed-Mukhram-Ullah MUJEEB <syed-mukhram-ullah.mujeeb@rntbci.com>
2025-01-12 07:04:23 +0530

    create order form completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './ui/Home'
 import Menu, {loader as menuLoader} from './features/menu/Menu'
 import Cart from './features/cart/Cart'
-import CreateOrder from './features/order/CreateOrder'
+import CreateOrder, {action as CreateOrderAction} from './features/order/CreateOrder'
 import Order, {loader as orderLoader} from './features/order/Order'
 import AppLayout from './ui/AppLayout'
 import Error from './ui/Error'
@@ -30,7 +30,8 @@ const router = createBrowserRouter([
       },
       {
         path: '/order/new',
-        element: <CreateOrder />
+        element: <CreateOrder />,
+        action: CreateOrderAction
       },
       {
         path: '/order/:orderId',
