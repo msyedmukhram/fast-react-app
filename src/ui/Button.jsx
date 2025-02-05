7ca095828486e8e80136f0c3dd591266206ee8c3
Syed-Mukhram-Ullah MUJEEB <syed-mukhram-ullah.mujeeb@rntbci.com>
2025-02-05 09:26:07 +0530

    bug fixes

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Button = ({disabled,children, to}) => {
     const className='bg-yellow-400 uppercase foont-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4'
     if(to){
-        return <Link to={to} className={className}></Link>
+        return <Link to={to} className={className}>{children}</Link>
     }
   return (
     <button disabled={disabled} className={className}>{children}</button>
--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const LinkButton = ({to,children}) => {
     const navigate = useNavigate();
